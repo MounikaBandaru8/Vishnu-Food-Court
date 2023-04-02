5f463ce9e0888712b51bfa9aaa3babb57fcdfc9b
MounikaBandaru8 <99631120+MounikaBandaru8@users.noreply.github.com>
2023-04-02 15:46:51 +0530

    Add files via upload

---
+++ partials-front/footer.php
@@ -0,0 +1,32 @@
+    <!-- social Section Starts Here -->
+    <section class="social">
+        <div class="container text-center">
+            <ul>
+               <!-- <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>-->
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="https://svecw.edu.in/">SVECW</a></p>
+        </div>
+    </section>
+    <!--<div class="wrapper">
+            <p class="text-center">2022 All rights reserved,SVECW.Developed by-<a href="www.svecw.edu.in">Team A6 </a></p>
+      </div>-->
+
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
